0fc44fe869b412c13090c97ebf972866e7c5ca24
Popped-Corn1 <etrahant1@gmail.com>
2023-11-08 17:23:59 -0600

    implemented sound toggle logic and fixed puzzleList

--- static/javascript/sound-effects.js
+++ static/javascript/sound-effects.js
@@ -1,33 +1,44 @@
 // sound-effects.js
 
-export const rightAnswerSound = new Audio('/static/soundeffects/correct-answer.wav');
-export const wrongAnswerSound = new Audio('/static/soundeffects/wrong-answer.wav');
+const rightAnswerSound = document.getElementById('rightAnswerSound');
+const wrongAnswerSound = document.getElementById('wrongAnswerSound');
+const soundEffectToggle = document.getElementById('sound-effects');
+soundEffectToggle.addEventListener('change', function () {
+    if (this.checked) {
+        enableSoundMode();
+    } else {
+        disableSoundMode();
+    }
+});
 
-// Function to toggle sound effects
-export function toggleSoundEffects() {
-    const soundEffectsToggle = document.querySelector('#soundEffectsToggle');
-
-    // Check the initial setting from local storage or set it to 'enabled' by default
-    const soundEffectsEnabled = localStorage.getItem('soundEffects') !== 'disabled';
-
-    // Update the toggle switch based on the setting
-    soundEffectsToggle.checked = soundEffectsEnabled;
-
-    // Update the sound effects based on the setting
-    rightAnswerSound.muted = !soundEffectsEnabled;
-    wrongAnswerSound.muted = !soundEffectsEnabled;
-
-    // Listen for changes in the toggle switch
-    soundEffectsToggle.addEventListener('change', () => {
-        const soundEffectsEnabled = soundEffectsToggle.checked;
-        rightAnswerSound.muted = !soundEffectsEnabled;
-        wrongAnswerSound.muted = !soundEffectsEnabled;
-
-        // Save the user's preference to local storage
-        if (soundEffectsEnabled) {
-            localStorage.setItem('soundEffects', 'enabled');
-        } else {
-            localStorage.setItem('soundEffects', 'disabled');
-        }
-    });
+function enableSoundMode() {
+    rightAnswerSound.muted = false;
+    wrongAnswerSound.muted = false;
+    localStorage.setItem('soundEffectToggle', 'enabled');
+    console.log("Enabling sounds");
 }
+
+function disableSoundMode() {
+    rightAnswerSound.muted = true;
+    wrongAnswerSound.muted = true;
+    localStorage.setItem('soundEffectToggle', 'disabled');
+    console.log("Disabling sounds");
+}
+
+const savedSoundEffectMode = localStorage.getItem('soundEffectToggle');
+    if (savedSoundEffectMode === 'enabled') {
+    enableSoundMode();
+    var soundEffectCheckbox = document.getElementById('sound-effects');
+
+    // Trigger the 'change' event
+    soundEffectCheckbox.checked = true; // Set it to true for checked, false for unchecked
+    soundEffectCheckbox.dispatchEvent(new Event('change'));
+    }
+    else if (savedSoundEffectMode === 'disabled') {
+        disableSoundMode();
+        var soundEffectCheckbox = document.getElementById('sound-effects');
+
+        // Trigger the 'change' event
+        soundEffectCheckbox.checked = false; // Set it to true for checked, false for unchecked
+        soundEffectCheckbox.dispatchEvent(new Event('change'));
+    }
