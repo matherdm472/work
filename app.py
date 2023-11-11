bf6c71284374f4c1cec7b073ff74708545e5a610
matherdm472 <74468791+matherdm472@users.noreply.github.com>
2023-11-10 18:42:34 -0600

    redepolying application

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    # Check if the request URL doesn't start with 'www.'
+    if request.url.startswith('http://thetweedle.com'):
+        # Redirect to the same path but with 'www.'
+        return redirect(request.url.replace('http://', 'http://www.', 1), code=301)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-#this is the basic code that all Flask uses
