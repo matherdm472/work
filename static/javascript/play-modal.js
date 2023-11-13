905b7f1e3eaddc6bc5c19687fd0ccee788c80b61
Popped-Corn1 <etrahant1@gmail.com>
2023-11-13 17:41:45 -0600

    update day

--- static/javascript/play-modal.js
+++ static/javascript/play-modal.js
@@ -17,7 +17,7 @@ export { tempDay };
 export { resetMenu };
 
 
-const startDate = new Date('2023-11-11');
+const startDate = new Date('2023-11-13');
 
 // Function to calculate the number of days passed
 function calculateDaysPassed() {
