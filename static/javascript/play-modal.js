58b16a83d13750bca94c45eb8f2457c3494e3eae
matherdm472 <74468791+matherdm472@users.noreply.github.com>
2024-03-07 20:04:55 -0600

    Update play-modal.js

--- static/javascript/play-modal.js
+++ static/javascript/play-modal.js
@@ -34,7 +34,7 @@ function calculateDaysPassed(startDate) {
     return daysPassed;
 }
 
-const startDate = new Date('2023-11-15');
+const startDate = new Date('2023-11-16');
 let day = calculateDaysPassed(startDate);
 let tempDay = day;
 
